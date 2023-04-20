71f94e213fcf40f6ffee73aa30f71bd50af3f9d2
naeemdeveloper <azharulislamnaeembd@gmail.com>
2023-04-20 11:38:24 +0600

    landing page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,6 @@
 (function ($) {
     
     "use strict"
-    $(document).ready(function () {
-
-        // mobile menu
-        $('#mobile-menu').meanmenu({
-            meanMenuContainer: '.mobile-menu',
-            meanScreenWidth: "991.98",
-            onePage: false
-        });
-
-    });
-
-    $(window).on('scroll', function () {
-
-        // back to top scroll
-        var ScrollTop = $('.back-to-top');
-
-        if ($(window).scrollTop() > 1000) {
-            ScrollTop.fadeIn(1000);
-        } else {
-            ScrollTop.fadeOut(1000);
-        }
-
-    });
 
     $(window).on('load', function () {
 
@@ -33,13 +10,6 @@
         // preloader
         var preLoder = $("#preloader");
         preLoder.fadeOut(0);
-
-        // back to top animate
-        $(".back-to-top").on('click', function () {
-            $("html").animate({
-                "scrollTop": "0"
-            }, 1000);
-        });
 
     });
 
